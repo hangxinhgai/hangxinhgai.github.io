5cb438a3f58cfeaea4c3e1bcc790a042d57d9088
Hà Mạnh Tín <hatin7080@gmail.com>
2021-11-06 23:13:31 +0700

    1

--- js/my_js.js
+++ js/my_js.js
@@ -1,3 +1,12 @@
+window.onload = function() {
+  var context = new AudioContext();
+  // Setup all nodes
+  // ...
+  document.getElementById("beep").pause();
+	document.getElementById('beep').muted = false;
+	document.getElementById("beep").play();
+}
+
 $(document).ready(function () {
 	document.getElementById("beep").pause();
 	document.getElementById('beep').muted = false;
