95e44cd2110910223c0715edd9923795c32c9776
Hà Mạnh Tín <hatin7080@gmail.com>
2021-11-06 23:48:50 +0700

    a

--- js/my_js.js
+++ js/my_js.js
@@ -2,16 +2,9 @@ window.onload = function() {
   //var context = new AudioContext();
   // Setup all nodes
   // ...
-  document.getElementById("beep").pause();
+    document.getElementById("beep").pause();
 	document.getElementById('beep').muted = false;
-	//document.getElementById("beep").play();
+	document.getElementById("beep").play();
 }
  
  
-  var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-  if (!isChrome){
-      $('#iframeAudio').remove()
-  }
-  else {
-      $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
-  }
